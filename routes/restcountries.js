bac2418d6a065c1c6a8cc74a7ceef17b68791b4f
ADEDAYO ADEGBOYE <adedayoadegboye@ADEDAYOs-Air.lan>
2020-04-17 11:53:46 +0300

    test server

--- routes/restcountries.js
+++ routes/restcountries.js
@@ -1,44 +1,41 @@
 const router = require("express").Router();
-const axios = require('axios');
-const fetch = require('node-fetch');
+const axios = require("axios");
+const fetch = require("node-fetch");
 
 router.post("/CountryByName", (req, res) => {
-    console.log(req.body);
-    
-    const countryName = req.body.country
+  console.log(req.body);
 
-    const baseUrl = `https://restcountries.eu/rest/v2/name/${countryName}`;
-	
-			fetch(baseUrl)
-			.then(res =>res.json())
-			.then(data => {
-			 res.send({ data });
-			})
-			.catch(err => {
-				if (err){
-					res.send({message:'Country not found'});
-				}
-				
-			});
-})
+  const countryName = req.body.country;
 
-router.post("/AllCountries", (req, res) => {
-    console.log('fired');
-    
-    const baseUrl = `https://restcountries.eu/rest/v2/all`;
-		
-    fetch(baseUrl)
+  const baseUrl = `https://restcountries.eu/rest/v2/name/${countryName}`;
+
+  fetch(baseUrl)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        
-     res.send({ data });
-     
+      res.send({ data });
     })
     .catch(err => {
-        res.send(err);
+      if (err) {
+        res.send({ message: "Country not found" });
+      }
     });
+});
 
-})
+router.post("/AllCountries", (req, res) => {
+  console.log("fired");
+
+  const baseUrl = `https://restcountries.eu/rest/v2/all`;
+
+  fetch(baseUrl)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+
+      res.send({ data });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
